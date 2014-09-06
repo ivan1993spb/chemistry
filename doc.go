3f55ac883b00fe28cc418e6ee82d8c00ec4137a4
Ivan Pushkin <pushkin13@bk.ru>
2014-09-07 00:41:26 +0400

    rewrited doc

--- doc.go
+++ doc.go
@@ -5,18 +5,21 @@ elements.
 
 For example you can get electron configuration scheme as map:
 
-	var electron_configuration = chemistry.ELEMENT_Kr.Levels()
+	var electron_configuration = chemistry.ELEMENT_Kr.ElectronConfiguration()
 
 And then print it in two forms:
 
 	// Full
 	fmt.Printf("for %q electron configuration is %q\n",
 		chemistry.ELEMENT_Kr,
-		chemistry.LevelsFullString(electron_configuration))
+		chemistry.ELEMENT_Kr.ElectronConfiguration())
 
 	// Short
 	fmt.Printf("short form: %q\n",
-		chemistry.LevelsShortString(electron_configuration))
+		chemistry.ELEMENT_Kr.ElectronConfiguration().Short())
+
+	// Electron count per shell
+	fmt.Println(chemistry.ELEMENT_Kr.ElectronConfiguration().EnergyPerShell())
 
 */
 package chemistry
